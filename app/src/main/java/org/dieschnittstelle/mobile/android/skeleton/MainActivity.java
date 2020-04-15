14369e7e0aa827596ac9e53b54ea800fa6045960
thalkad <kadir.thal@gmail.com>
2020-04-15 22:09:40 +0200

    Uebung nachgebaut. ListenElemente und ActionBar angelegt und ausgerichtet. ClickEvent mit einem Toast versehen.

--- app/src/main/java/org/dieschnittstelle/mobile/android/skeleton/MainActivity.java
+++ app/src/main/java/org/dieschnittstelle/mobile/android/skeleton/MainActivity.java
@@ -1,10 +1,13 @@
 package org.dieschnittstelle.mobile.android.skeleton;
 
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-import org.dieschnittstelle.mobile.android.skeleton.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewGroup listView = this.findViewById(R.id.listView);
+        FloatingActionButton fab = this.findViewById(R.id.fab);
+
+        for (int i = 0; i < listView.getChildCount(); i++) {
+            TextView currentChild = (TextView) listView.getChildAt(i);
+            currentChild.setOnClickListener((view) -> {
+                this.showFeedbackMessage("List Element: " + currentChild.getText() + "!");
+            });
+        }
+
+        fab.setOnClickListener((view) -> {
+            this.showFeedbackMessage("Add new Element");
+        });
     }
+
+    private void showFeedbackMessage(String msg) {
+        Toast.makeText(this, "Click on: " + msg, Toast.LENGTH_SHORT).show();
+    }
+
 }
